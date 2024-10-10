3db541ac9ffa569af6e884afb7b7461f224d69b1
IshiniAponso <ishiniaponso@gmail.com>
2024-10-10 09:32:55 +0530

    feat: enableCors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: 'http://localhost:8081',  // Allow requests only from this frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(6340);
 }
 bootstrap();
