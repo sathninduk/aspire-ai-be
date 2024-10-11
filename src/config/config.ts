afdf967d0ba5882e869cc76d56466e7ea6e8dd1f
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-10-11 10:22:53 +0530

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 const config = {
   mongoDB: {
-    url: 'mongodb://localhost:27017/aspireai',
-    // url: 'mongodb+srv://sathnidukottage:C2ly3Xe72jQc7Pxu@devcluster.smp9t.mongodb.net/aspireai?retryWrites=true&w=majority&appName=DevCluster',
+    // url: 'mongodb://localhost:27017/aspireai',
+    url: 'mongodb+srv://sathnidukottage:C2ly3Xe72jQc7Pxu@devcluster.smp9t.mongodb.net/aspireai?retryWrites=true&w=majority&appName=DevCluster',
   },
   googleJobs: {
     clientId: '',
