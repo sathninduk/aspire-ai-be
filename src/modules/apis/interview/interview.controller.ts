3e66ea0723ab09f869c65628ad2d7733d70c010d
IshiniAponso <ishiniaponso@gmail.com>
2024-10-12 00:30:17 +0530

    feat: interview controller , service
    feat: interview controller , service

--- src/modules/apis/interview/interview.controller.ts
+++ src/modules/apis/interview/interview.controller.ts
@@ -1,24 +1,13 @@
-import { Controller, Post, Req, Res, Body } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { InterviewService } from './interview.service';
-import { Response } from 'express';
 
 @Controller('interview')
 export class InterviewController {
   constructor(private readonly interviewService: InterviewService) {}
 
   @Post('questions')
-  async getInterviewQuestions(
-    @Res() res: Response,
-    @Body() body: { jobKeyword: string },  // Get jobKeyword from request body
-  ): Promise<void> {
-    const { jobKeyword } = body;
-
-    try {
-      const AIResponse = await this.interviewService.getQuestions(jobKeyword);
-      res.send(AIResponse);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('An error occurred while fetching interview questions');
-    }
+  async getInterviewQuestions(@Body('jobKeyword') jobKeyword: string) {
+    const questions = await this.interviewService.getQuestions(jobKeyword);
+    return { questions }; 
   }
 }
