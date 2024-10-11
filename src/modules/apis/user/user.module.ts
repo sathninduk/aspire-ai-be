23f7c5c642e40837f0513077c78819845e95f03a
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-10-11 10:18:54 +0530

    feat: onboard details

--- src/modules/apis/user/user.module.ts
+++ src/modules/apis/user/user.module.ts
@@ -1,8 +1,14 @@
+// src/modules/apis/user/user.module.ts
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserService } from './user.service';
+import { User, UserSchema } from './user.schema';
+import { UserController } from './user.controller';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
